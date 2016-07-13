97bbbbf333a627462ccac9459a011ada859b3055
guozhaokui <guozk@layabox.com>
2016-07-13 13:10:29 +0800

    加了一个 -cout 参数直接控制输出路径 如果不用 -cout 缺省输出改成了资源目录下。dcc会忽略layadcc目录 加了个输出打印。 url可以不完整

--- bin/exe.js
+++ bin/exe.js
@@ -8,6 +8,7 @@ if (process.argv.length < 3) {
     console.log('       -cache 生成cache.');
     console.log('       -lwr 文件路径全部转为小写。');
     console.log('       -url url 生成cache的话，对应的url.');
+    console.log('       -cout outpath cache的输出目录，如果不设置的话，就是在资源目录下。');
     console.log('例如:');
     console.log('   layadcc d:/game/wow -cache -url www.game.com');
     process.exit(1);
@@ -26,6 +27,8 @@ process.argv.forEach((v,i,arr)=>{
         case '-url':
             options.url=arr[i+1];
             break;
+        case '-cout':
+            options.cout=arr[i+1];
         }
     }
 });
@@ -36,14 +39,28 @@ if(!fs.existsSync(srcpath)){
     process.exit(1);
 }
 
+var relout=false;   //输出在资源目录下
 var outpath = srcpath + '/update';
 if( options.url && options.cache){
-    outpath = path.resolve(process.cwd(),'layadccout');
+    if(options.cout){
+        if(path.isAbsolute(options.cout)) outpath = options.cout;
+        else{
+            if(options.cout.split(/[\\\/]/).length>1){
+                console.log('错误：输出路径如果是相对路径，不允许多层，当前输出路径不满足这个条件:'+options.cout);
+                process.exit(1);
+            }
+            relout=true;
+            outpath = path.resolve(process.cwd(),options.cout);
+        }
+    }
+    else outpath = path.resolve(/*process.cwd()*/srcpath,'layadccout');  //好多人不知道当前目录是什么，改成缺省为资源目录
 }
 if (!fs.existsSync(outpath)) {
-    console.log('输出目录'+outpath+' 不存在，创建一个.');
+    //console.log('输出目录'+outpath+' 不存在，创建一个.');
     fs.mkdirSync(outpath);
 }
 
 var gendcc = require('../lib/genDcc');
 gendcc.gendcc (srcpath, outpath, options.cache, options.lwr, options.url); 
+if(relout)
+    console.log('注意:\n当前输出路径在资源目录下，记得下次生成cache的时候删除这个输出目录('+outpath+')，否则也会被算作资源处理的。')
